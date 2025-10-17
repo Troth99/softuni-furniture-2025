824ee8d56ebe6ff61e56a79c02164bebff853592
Troth99 <trothlol123456@gmail.com>
2025-10-17 04:19:48 +0300

    Add express to the api

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,15 @@
 console.log('It works')
+
+import express from 'express'
+
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    console.log('it works')
+    res.send('It works!')
+})
+
+
+app.listen(3030, () => console.log('Server is listening on http://localhost:3030/...') )
